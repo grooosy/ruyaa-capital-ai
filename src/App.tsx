79474dd55da80538870252dcf8b09cff329dbdfb
Taher-Fixora Ai business solutions <taher-samman@hotmail.com>
2025-06-22 23:58:12 +0400

    Fix syntax error in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,13 @@ import WelcomePage from "./pages/Welcome";
 import { Routes, Router } from 'react-router-dom';
 
 const App = () => {
-  return (\n    <Router>\n      <Routes as="appRoutes">\n        <route path="/welcome" element={ <WelcomePage /> } />\n      </Routes>\n    </Router>\n  );
+  return (
+    <Router>
+      <Routes as="appRoutes">
+        <route path="/welcome" element={<WelcomePage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
