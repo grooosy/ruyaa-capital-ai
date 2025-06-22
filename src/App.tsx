0c60e96da714ebf5ebab397fe1b910256142e5e0
Taher-Fixora Ai business solutions <taher-samman@hotmail.com>
2025-06-23 00:01:27 +0400

    Fix React Router errors in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import WelcomePage from "./pages/Welcome";
-import { Routes, Router } from 'react-router-dom';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
 
 const App = () => {
   return (
-    <Router>
-      <Routes as="appRoutes">
-        <route path="/welcome" element={<WelcomePage />} />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/welcome" element={<WelcomePage />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 };
 
