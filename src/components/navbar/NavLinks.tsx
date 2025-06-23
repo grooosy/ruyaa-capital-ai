8147a684e5d52ab3907adcf12817c98cc6ba6a6f
Taher-Fixora Ai business solutions <taher-samman@hotmail.com>
2025-06-23 14:25:17 +0400

    Step 2: Add NavLinks component for modular navbar design

---
+++ src/components/navbar/NavLinks.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+function NavLinks() {
+  return (
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/dashboard">Dashboard</a></li>
+      <li><a href="/academy">Academy</a></li>
+    </ul>
+  );
+}
+
+export default NavLinks;
