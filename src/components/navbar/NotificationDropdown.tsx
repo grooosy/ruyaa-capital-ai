304bab760f6de030b303a31198ee90679abd2475
Taher-Fixora Ai business solutions <taher-samman@hotmail.com>
2025-06-23 14:25:41 +0400

    Step 2: Add NotificationDropdown component to navbar module

---
+++ src/components/navbar/NotificationDropdown.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+function NotificationDropdown() {
+  return (
+    <div>
+      <button>Notifications</button>
+    </div>
+  );
+}
+
+export default NotificationDropdown;
